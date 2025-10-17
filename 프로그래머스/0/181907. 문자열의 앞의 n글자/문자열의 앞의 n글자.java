a450185b347612961a9d698da31b0bb88ca3dfa7
Park Gyeongwoon <65270711+gyngxn@users.noreply.github.com>
2025-10-17 20:36:38 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        
+        String front = my_string.substring(0,n);
+        
+        answer = front;
+        
+        return answer;
+    }
+}
