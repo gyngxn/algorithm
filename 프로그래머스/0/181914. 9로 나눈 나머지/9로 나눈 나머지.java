aa6cab2fbf0fe7ac1d2f4b00a9a9897f7291f3e6
Park Gyeongwoon <65270711+gyngxn@users.noreply.github.com>
2025-10-30 00:27:54 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 5.58 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        int sum = 0;
+        for(int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            sum += c - '0';
+        }
+        
+        return sum % 9;
+    }
+}
