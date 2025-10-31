74adfbbc6bef8ca877c4f668ca0b5743f3af14a9
Park Gyeongwoon <65270711+gyngxn@users.noreply.github.com>
2025-10-31 14:26:43 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int num;
+        
+        for(int i = 0; i < num_list.length; i++) {
+            num = num_list.length - (i+1);
+            answer[i] = num_list[num];
+        }
+        return answer;
+    }
+}
