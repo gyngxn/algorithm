da52fdc686b2fc362fdfbceb532f4ec5e1730f8a
Park Gyeongwoon <65270711+gyngxn@users.noreply.github.com>
2025-11-02 20:00:44 +0900

    [level 0] Title: 배열 만들기 1, Time: 7.35 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int k) {
+         ArrayList<Integer> answerList = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i++) {
+            if(i % k == 0) {
+                answerList.add(i);
+            }    
+        }
+        int[] answer = new int[answerList.size()];
+        for(int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+        return answer;
+    }
+}
