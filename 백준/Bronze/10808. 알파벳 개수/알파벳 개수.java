55a3f6aacd82977ed2ec91b25f5b397bb2679797
Park Gyeongwoon <65270711+gyngxn@users.noreply.github.com>
2025-12-04 15:41:20 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 128 ms, Memory: 15848 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine().trim();
+		int alb[] = new int[26];
+		
+		for(int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			
+			if(c >= 'a' && c <= 'z') {
+				alb[c - 'a']++;
+			}
+		}
+		
+		for(int i = 0; i < alb.length; i++) {
+			System.out.print(alb[i] + " ");
+		}
+	}
+}
