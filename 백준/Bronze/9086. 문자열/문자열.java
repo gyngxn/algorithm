aa64092bfd92a56831903bec2e115acdabb22dd8
Park Gyeongwoon <65270711+gyngxn@users.noreply.github.com>
2025-12-05 16:14:36 +0900

    [Bronze V] Title: 문자열, Time: 124 ms, Memory: 15828 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			String str = br.readLine();
+			
+			char front = str.charAt(0);
+			char back = str.charAt(str.length() - 1);
+			
+			System.out.println(front + "" + back);
+		}
+	}
+}
