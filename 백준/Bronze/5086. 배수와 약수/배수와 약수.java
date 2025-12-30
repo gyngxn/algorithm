1973458b449af48d17f779694d80b08cb1e56ca7
Park Gyeongwoon <65270711+gyngxn@users.noreply.github.com>
2025-12-30 15:28:56 +0900

    [Bronze III] Title: 배수와 약수, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			
+			int first = Integer.parseInt(st.nextToken());
+			int second = Integer.parseInt(st.nextToken());
+			
+			if(first == 0 && second == 0) break;
+			
+			if(second % first == 0) {
+				System.out.println("factor");
+			} else if (first % second == 0) {
+				System.out.println("multiple");
+			} else {
+				System.out.println("neither");
+			}
+		}
+	}
+}
